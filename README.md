83a8b09ded35c58caaea1a45a4873c1ac8e98799
The Goose <ryanshaffer1324@gmail.com>
2024-06-05 19:36:37 -0400

    Added some thoughts to js and also a source of inspiration to readme. will go over inspo and then begin tackling js portion of assignment.

--- README.md
+++ README.md
@@ -20,3 +20,5 @@ WHEN I view future weather conditions for that city
 THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 WHEN I click on a city in the search history
 THEN I am again presented with current and future conditions for that city
+
+Major help and inspiration from... https://www.youtube.com/watch?v=74IOjtVvExY
---
+++ assets/script.js
@@ -0,0 +1,11 @@
+const locationSearch = document.getElementById('#location-search').textContent
+const searchBtn = document.getElementById('#form-search-btn')
+const pastSearches = document.getElementById('#past-searches')
+
+// so when a user inputs data, i will need to run an api call based on the locaton they type in - think this will require both the weather api and coordinates api
+
+function locationSearchFx () {
+  // when i search a location i want the data correspodning to that location to populate in the main city container and also the five day container
+  // i will need functions to do those as well
+  // i think i will also need to fetch location data via an api pull
+}
